e59716af882c702ca559631a323ef22a15843dd7
luckypeppa <871565198@qq.com>
2023-01-28 20:11:19 +0800

    chore: modified usage introduction

--- src/index.ts
+++ src/index.ts
@@ -62,7 +62,7 @@ if (agrv._[0] === 'proxy') {
 		}, execCallback)
 	} else {
 		console.error('A command must be provided.')
-		console.log(`Usage:
+		console.log(`SYNOPSIS:
 		sp [command]     // run command ith http_proxy and https_proxy set
 		sp proxy [proxy] // set proxy
 		sp proxy         // show proxy
